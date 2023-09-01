cfd613cdb22166f4cbab8126bdb36dfc0b3d2bfe
Zach Riane Machacon <zachriane01@gmail.com>
2023-09-02 07:27:16 +0800

    [FEAT] Added recommender and requirements file

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import argparse
 import logging
 import datetime as dt
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
 from pathlib import Path
+from dotenv import load_dotenv
+from scraper import get_recommendations
 
 # Flow:
 # 1. User enters args. 
@@ -13,7 +17,7 @@ from pathlib import Path
 #     2b. Return error 
 # 3. END
 
-# LOGGING_DIR = f'./logs/spotify_scrape-{dt.datetime.today().strftime("%Y%m%d")}.log'
+# LOGGING_DIR = f'logs/spotify_scrape-{dt.datetime.today().strftime("%Y%m%d")}.log'
 
 # logging.basicConfig(
 #     level=logging.INFO,
@@ -25,18 +29,21 @@ from pathlib import Path
 # )
 
 def main():
+    load_dotenv()
     parser = argparse.ArgumentParser(prog="Spotify Genre Track Scraper", 
                                      description="Gathers spotify tracks' features from Spotify API using seeds",
                                      epilog="Script made by @blurridge || Zach Riane I. Machacon"
                                      )
-    parser.add_argument("-g", "--genre", nargs='+', required=True, type=lambda s: [genre for genre in s.split(',')]
-                        help="The genre the recommender will use to scrape tracks")
-    parser.add_argument("-a", "--artist", nargs='+', required=True, type=lambda s: [artist for artist in s.split(',')]
-                        help="The artist the recommender will use to scrape tracks")
+    parser.add_argument("-g", "--genre", required=True, type=lambda s: s.split(","),
+                        help="The genre the recommender will use to scrape tracks. Comma separate different genres.")
+    parser.add_argument("-a", "--artist", required=True, type=lambda s: s.split(","),
+                        help="""The artist the recommender will use to scrape tracks. Comma separate different Spotify IDs. 
+                        Use Spotify IDs of artist. Example: 2KC9Qb60EaY0kW4eH68vr3""")
     parser.add_argument("-l", "--limit", type=int, required=True, 
                         help="The number of tracks the recommender will scrape")
     args = parser.parse_args()
-    print(args)
+    client = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
+    recommended_songs = get_recommendations(client=client, genres=args.genre, artists=args.artist, limit=args.limit)
 
 if __name__ == '__main__':
-    main()
+    main()  
--- scraper.py
+++ scraper.py
@@ -1,1 +1,3 @@
-import spotipy
+def get_recommendations(client, genres, artists, limit):
+    songs = client.recommendations(seed_artists=artists, seed_genres=genres, limit=limit)
+    return songs
